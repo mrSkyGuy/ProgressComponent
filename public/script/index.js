13aa9ec2869fb533ca50176d63ee4fe1e5a146df
SkyGuy <nurmagomedoffzaur@yandex.ru>
2024-07-18 17:31:32 +0300

    add dark mode

--- public/script/index.js
+++ public/script/index.js
@@ -1,9 +1,10 @@
 var _a;
 import getProgressComponent from "./progressComponent/progressComponent.js";
+const main = document.querySelector(".main");
 const valueInput = document.querySelector(".controller__text-input");
 const animateInput = document.querySelector(".switch__input_animate");
 const hideInput = document.querySelector(".switch__input_hide");
-const main = document.querySelector(".main");
+const darkModeInput = document.querySelector(".switch__input_dark-mode");
 const { progressComponent, setPercentage, setAnimate, setHide } = getProgressComponent("main__progress", {
     percentage: Number((_a = valueInput.getAttribute("value")) !== null && _a !== void 0 ? _a : 0),
     isAnimated: animateInput.hasAttribute("checked"),
@@ -27,5 +28,14 @@ hideInput.addEventListener("input", event => {
     if (input === null)
         return;
     setHide(input.checked);
+});
+darkModeInput.addEventListener("input", event => {
+    const input = event.target;
+    if (input === null)
+        return;
+    if (input.checked)
+        document.body.classList.add("dark-mode");
+    else
+        document.body.classList.remove("dark-mode");
 });
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import getProgressComponent from "./progressComponent/progressComponent.js";
 
+const main = document.querySelector(".main")!;
 const valueInput = document.querySelector(".controller__text-input")!;
 const animateInput = document.querySelector(".switch__input_animate")!;
 const hideInput = document.querySelector(".switch__input_hide")!;
-const main = document.querySelector(".main")!;
+const darkModeInput = document.querySelector(".switch__input_dark-mode")!;
 
 const {progressComponent, setPercentage, setAnimate, setHide} =
   getProgressComponent("main__progress", {
@@ -33,4 +34,12 @@ hideInput.addEventListener("input", event => {
   if (input === null) return;
 
   setHide(input.checked);
+});
+
+darkModeInput.addEventListener("input", event => {
+  const input = event.target as HTMLInputElement | null;
+  if (input === null) return;
+
+  if (input.checked) document.body.classList.add("dark-mode");
+  else document.body.classList.remove("dark-mode");
 });
